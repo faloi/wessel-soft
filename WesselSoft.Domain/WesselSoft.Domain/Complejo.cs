d4cb915541673e26c8764291396572287802f97d
Federico <federico.aloi@gmail.com>
2013-04-10 16:38:54 -0300

    Agregada clase Complejo

---
+++ WesselSoft.Domain/WesselSoft.Domain/Complejo.cs
@@ -0,0 +1,14 @@
+﻿namespace WesselSoft.Domain
+{
+    public class Complejo
+    {
+        public double ParteReal { get; set; }
+        public double ParteImaginaria { get; set; }
+
+        public Complejo(double parteReal, double parteImaginaria)
+        {
+            this.ParteReal = parteReal;
+            this.ParteImaginaria = parteImaginaria;
+        }
+    }
+}
