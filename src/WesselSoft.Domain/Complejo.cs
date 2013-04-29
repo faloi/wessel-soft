63c151a66a9aebf0fc1a78e1440f388e607983c5
Rodrigo Alfonso <rodri042@gmail.com>
2013-04-29 00:07:57 -0300

    Helpers

--- src/WesselSoft.Domain/Complejo.cs
+++ src/WesselSoft.Domain/Complejo.cs
@@ -15,6 +15,7 @@ namespace WesselSoft.Domain
         public double Modulo { get {
             return Math.Sqrt(Math.Pow(this.ParteReal, 2) + Math.Pow(this.ParteImaginaria, 2));
         } }
+
         public double Argumento { get {
             if (this.EsNulo)
                 throw new ComplejoNuloException("No se puede calcular el argumento del complejo nulo");
@@ -27,36 +28,41 @@ namespace WesselSoft.Domain
         public Complejo Conjugado { get {
             return Complejo.DesdeFormaBinomica(this.ParteReal, -this.ParteImaginaria);
         } }
-        public bool EsNulo { get { return this.ParteReal == 0 && this.ParteImaginaria == 0; } }
 
-        public string ToString(Representacion representacion) {
-            switch(representacion) {
-                case Representacion.Binomica:
-                    var parteReal = this.ParteReal.ToString(FORMATO_NUMERO);
-                    var signo = ParteImaginaria > 0 ? "+" : "-";
-                    var parteImaginaria = Math.Abs(this.ParteImaginaria).ToString(FORMATO_NUMERO);
+        public bool EsNulo { get {
+            return this.ParteReal == 0 && this.ParteImaginaria == 0;
+        } }
 
-                    var completo = this.ParteReal != 0 && this.ParteImaginaria != 0;
-                    var format = new StringBuilder()
-                        .Append(this.ParteReal != 0 || this.EsNulo ? "{0}" : "")
-                        .Append(completo || this.ParteImaginaria < 0 ? " {1} " : "")
-                        .Append(this.ParteImaginaria != 0 ? "{2}j" : "")
-                        .ToString();
-                    return String.Format(format, parteReal, signo, parteImaginaria);
-
-                case Representacion.Polar:
-                    var modulo = this.Modulo.ToString(FORMATO_NUMERO);
-                    try {
-                        var argumento = this.Argumento.ToString(FORMATO_NUMERO);
-                        return String.Format("[{0}; {1}]", modulo, argumento);
-                    } catch(ComplejoNuloException) {
-                        return "[Complejo nulo]";
-                    }
-            }
-            return base.ToString();
+        public Complejo ElevarA(uint natural) {
+            return Complejo.DesdeFormaPolar(Math.Pow(this.Modulo, natural), natural * this.Argumento);
         }
 
-        #region Operaciones
+        public Complejo LogaritmoNatural() {
+            return Math.Log(this.Modulo) + this.Argumento * Complejo.UnidadImaginaria();
+        }
+
+        #region Constructores
+            public static Complejo DesdeFormaBinomica(double parteReal, double parteImaginaria)
+            {
+                return new Complejo { ParteReal = parteReal, ParteImaginaria = parteImaginaria };
+            }
+
+            public static Complejo DesdeFormaPolar(double modulo, double argumento) {
+                var parteReal = modulo * Math.Cos(argumento);
+                var parteImaginaria = modulo * Math.Sin(argumento);
+                return Complejo.DesdeFormaBinomica(parteReal, parteImaginaria);
+            }
+
+            public static Complejo Nulo() {
+                return Complejo.DesdeFormaBinomica(0, 0);
+            }
+
+            public static Complejo UnidadImaginaria() {
+                return Complejo.DesdeFormaBinomica(0, 1);
+            }
+        #endregion
+
+        #region Helpers
             public static Complejo operator +(Complejo c1, Complejo c2) {
                 return Complejo.DesdeFormaBinomica(c1.ParteReal + c2.ParteReal, c1.ParteImaginaria + c2.ParteImaginaria);
             }
@@ -71,21 +77,48 @@ namespace WesselSoft.Domain
                 return Complejo.DesdeFormaPolar(c1.Modulo / c2.Modulo, c1.Argumento - c2.Argumento);
             }
 
-            public Complejo ElevarA(uint natural) {
-                return Complejo.DesdeFormaPolar(Math.Pow(this.Modulo, natural), natural * this.Argumento);
+            public static Complejo operator +(double r, Complejo c) {
+                var real = Complejo.DesdeFormaBinomica(r, 0);
+                return c + real;
             }
-        #endregion
-
-        #region Constructores
-            public static Complejo DesdeFormaBinomica(double parteReal, double parteImaginaria)
-            {
-                return new Complejo { ParteReal = parteReal, ParteImaginaria = parteImaginaria };
+            public static Complejo operator -(double r, Complejo c) {
+                var real = Complejo.DesdeFormaBinomica(r, 0);
+                return c - real;
+            }
+            public static Complejo operator *(double r, Complejo c) {
+                var real = Complejo.DesdeFormaBinomica(r, 0);
+                return c * real;
+            }
+            public static Complejo operator /(double r, Complejo c) {
+                var real = Complejo.DesdeFormaBinomica(r, 0);
+                return c / real;
             }
 
-            public static Complejo DesdeFormaPolar(double modulo, double argumento) {
-                var parteReal = modulo * Math.Cos(argumento);
-                var parteImaginaria = modulo * Math.Sin(argumento);
-                return Complejo.DesdeFormaBinomica(parteReal, parteImaginaria);
+            public string ToString(Representacion representacion) {
+                switch (representacion) {
+                    case Representacion.Binomica:
+                        var parteReal = this.ParteReal.ToString(FORMATO_NUMERO);
+                        var signo = ParteImaginaria > 0 ? "+" : "-";
+                        var parteImaginaria = Math.Abs(this.ParteImaginaria).ToString(FORMATO_NUMERO);
+
+                        var completo = this.ParteReal != 0 && this.ParteImaginaria != 0;
+                        var format = new StringBuilder()
+                            .Append(this.ParteReal != 0 || this.EsNulo ? "{0}" : "")
+                            .Append(completo || this.ParteImaginaria < 0 ? " {1} " : "")
+                            .Append(this.ParteImaginaria != 0 ? "{2}j" : "")
+                            .ToString();
+                        return String.Format(format, parteReal, signo, parteImaginaria);
+
+                    case Representacion.Polar:
+                        var modulo = this.Modulo.ToString(FORMATO_NUMERO);
+                        try {
+                            var argumento = this.Argumento.ToString(FORMATO_NUMERO);
+                            return String.Format("[{0}; {1}]", modulo, argumento);
+                        } catch (ComplejoNuloException) {
+                            return "[Complejo nulo]";
+                        }
+                }
+                return base.ToString();
             }
         #endregion
     }
