d56dbec279795c99c61550d6f484979a74d51b0c
Rodrigo Alfonso <rodri042@gmail.com>
2013-05-20 23:20:31 -0300

    Función Transferencia. Listo

---
+++ src/WesselSoft.Domain/FuncionTransferencia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WesselSoft.Domain
+{
+    public class FuncionTransferencia
+    {
+        public double K { get; set; }
+        public IList<Complejo> Ceros { get; protected set; }
+        public IList<Complejo> Polos { get; protected set; }
+
+        public FuncionTransferencia(double k, IList<Complejo> ceros, IList<Complejo> polos) {
+            this.K = k;
+            this.Ceros = ceros;
+            this.Polos = polos;
+        }
+
+        public Complejo EvaluarEn(Complejo complejo) {
+            return
+                K * Ceros.Aggregate((a, b) => a * (complejo - b))
+//                  _____________________________________________
+                /   Polos.Aggregate((a, b) => a * (complejo - b));
+        }
+    }
+}
